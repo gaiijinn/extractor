650901205eb1e4a3328cd16f353fec63a7b91298
bity3pip <egor.sh23@gmail.com>
2024-11-21 19:38:28 +0100

    some upd

--- email_parser.py
+++ email_parser.py
@@ -1,28 +1,28 @@
+from parser.csv_reader import CSVParser
 from extract_emails import DefaultFilterAndEmailFactory as Factory
 from extract_emails import DefaultWorker
 from extract_emails.browsers.requests_browser import RequestsBrowser
 import concurrent.futures
+import time
+from extract_emails.browsers.page_source_getter import PageSourceGetter
 from typing import Type
 from parser import chunkers, mixins
 import abc
 
 
 class BaseFastProcessor(abc.ABC, mixins.ThreadMixin, mixins.ProcessMixin):
-    def __init__(self, data, threads: int = 1, processes: int = 1,
-                 chunker_class: Type[chunkers.BaseChunker] = chunkers.SimpleChunker,
-                 **kwargs):
-        self.data = data
+    def __init__(self, threads: int = 1, processes: int = 1,
+                 chunker_class: Type[chunkers.BaseChunker] = chunkers.SimpleChunker):
         self.threads = threads
         self.processes = processes
         self.chunker = chunker_class(self.processes)
-        self.extra_args = kwargs
 
     @abc.abstractmethod
     def worker_function(self, item):
         pass
 
-    def process_data(self):
-        chunks = self.chunker.chunk_data(self.data)
+    def process_data(self, data):
+        chunks = self.chunker.chunk_data(data)
         results = self.process_in_processes(
             chunks, self._process_chunk, max_processes=self.processes
         )
@@ -41,32 +41,32 @@ class BaseFastProcessor(abc.ABC, mixins.ThreadMixin, mixins.ProcessMixin):
     def _process_chunk(self, chunk):
         return self.process_in_threads(chunk, self.worker_function, max_threads=self.threads)
 
-    def run(self):
-        return self.process_data()
-
 
 class WebsiteProcessor(BaseFastProcessor):
-    def __init__(self, websites, **kwargs):
-        super().__init__(websites, **kwargs)
-        self.browser = kwargs.get("browser", RequestsBrowser)()
+    def __init__(self, websites, threads: int = 1, processes: int = 1,
+                 chunker_class: Type[chunkers.BaseChunker] = chunkers.SimpleChunker,
+                 browser: Type[PageSourceGetter] = RequestsBrowser):
+        super().__init__(threads, processes, chunker_class)
+        self.websites = websites
+        self.browser = browser()
 
     def worker_function(self, website):
         factory = Factory(website_url=website, browser=self.browser, depth=5, max_links_from_page=1)
         worker = DefaultWorker(factory)
         return worker.get_data()
 
+    def run(self):
+        return self.process_data(self.websites)
 
 
 if __name__ == '__main__':
-    # file = CSVReader(file_path='crunchbase_data/small_data.csv', row_name='homepage_url')
-    # websites = file.read_file()
-    #clean_websites = [email for email in websites if email == 'investor']
+    file = CSVParser(file_path='crunchbase_data/test.csv', row_name='homepage_url')
+    websites = file.get_data()
 
-    # time_start = time.time()
-    # processor_simple = WebsiteProcessor(websites=websites, threads=1, processes=2)
-    # result_simple = processor_simple.run()
-    # time_end = time.time()
-    # print(result_simple)
-    # print(time_end-time_start)
-    # print(len(result_simple))
-    pass
+    time_start = time.time()
+    processor_simple = WebsiteProcessor(websites=websites, threads=70, processes=3)
+    result_simple = processor_simple.run()
+    time_end = time.time()
+    print(result_simple)
+    print(time_end-time_start)
+    print(len(result_simple))
