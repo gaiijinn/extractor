80e1da072d69cbbb0e94fe583bebfca57911e6ad
Illa Tsiluiko <helperfor@ukr.net>
2024-11-25 19:29:45 +0200

    add class EmailDomenFilter

--- extractClasses.py
+++ extractClasses.py
@@ -4,6 +4,15 @@ import csv
 import abc
 
 
+class EmailProcessorInterface(abc.ABC):
+    @abc.abstractmethod
+    def load_emails(self) -> None:
+        pass
+
+    @abc.abstractmethod
+    def save_emails_to_csv(self) -> None:
+        pass
+
 class BaseEmailValidator(abc.ABC):
     def __init__(self, pattern):
         self.pattern = pattern
@@ -22,7 +31,7 @@ class EmailValidator(BaseEmailValidator):
         return None
 
 
-class RemoveDuplicatesEmails:
+class RemoveDuplicatesEmails(EmailProcessorInterface):
     def __init__(self, input_path: str, output_path: str, validator: EmailValidator = None):
         self.input_path = input_path
         self.output_path = output_path
@@ -61,9 +70,36 @@ class RemoveDuplicatesEmails:
         self.save_emails_to_csv()
 
 
-test = RemoveDuplicatesEmails(input_path='thread_result/just_all_data_2procc_20thread_5depth.csv',
-                              output_path='final_test.csv',)
+class EmailDomainFilter(RemoveDuplicatesEmails):
+    def filter_emails(self, email_part: str = None, domain_part: str = None) -> dict:
+        filtered_data = {}
 
-test.start_remove_duplicates_emails()
-my_data = test.website_emails
-print(my_data)
+        for website, emails in self.website_emails.items():
+            filtered_emails = set()
+            for email in emails:
+                local_part, domain = email.split("@")
+                if (
+                    (email_part is None or email_part in local_part) and
+                    (domain_part is None or domain_part in domain)
+                ):
+                    filtered_emails.add(email)
+
+            if filtered_emails:
+                filtered_data[website] = filtered_emails
+
+        self.website_emails = filtered_data
+
+
+# test = RemoveDuplicatesEmails(input_path='thread_result/just_all_data_2procc_20thread_5depth.csv',
+#                               output_path='final_test.csv',)
+#
+# test.start_remove_duplicates_emails()
+# my_data = test.website_emails
+# print(my_data)
+
+email_filter = EmailDomainFilter(input_path='final_test.csv', output_path='final_test2.csv')
+email_filter.load_emails()
+filtered_emails = email_filter.filter_emails(email_part="support", domain_part="gmail.com")
+# filtered_emails = email_filter.filter_emails(domain_part="gmail.com")
+# filtered_emails = email_filter.filter_emails(email_part="support")
+email_filter.save_emails_to_csv()
