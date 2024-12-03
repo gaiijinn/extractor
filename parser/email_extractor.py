6fbba2dec1875e0dcfdcbd22bedba6dfa06059ba
gaiijinn <vladruban8@gmail.com>
2024-12-03 20:03:39 +0200

    some speed)

--- parser/email_extractor.py
+++ parser/email_extractor.py
@@ -1,5 +1,6 @@
 import subprocess
 from abc import ABC, abstractmethod
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from parser_helpers.installer.email_extractor_installer import CurlInstaller
 
@@ -11,10 +12,11 @@ class BaseEmailExtractor(ABC):
 
 
 class EmailExtractor(CurlInstaller, BaseEmailExtractor):
-    def __init__(self, output_file: str, data):
+    def __init__(self, output_file: str, data, num_threads: int = 100):
         self.output_file = output_file
         self.results = {}
         self._data = data
+        self.num_threads = num_threads
 
     def extract_emails_from_url(self, homepage_url: str) -> list:
         try:
@@ -31,25 +33,36 @@ class EmailExtractor(CurlInstaller, BaseEmailExtractor):
             )
             with open(self.output_file, mode="r", encoding="utf-8") as f:
                 emails = f.read().strip().split("\n")
+                print([email for email in emails if email])
                 return [email for email in emails if email]
         except subprocess.CalledProcessError as e:
             print(f"Error processing {homepage_url}: {e}")
         return []
 
-    def process_csv(self):
-        for row in self._data:
-            uuid = row.get("uuid", "").strip()
-            homepage_url = row.get("homepage_url", "").strip()
+    def _process_row(self, row):
+        uuid = row.get("uuid", "").strip()
+        homepage_url = row.get("homepage_url", "").strip()
 
-            if homepage_url:
-                print(f"Processing {homepage_url}")
-                emails = self.extract_emails_from_url(homepage_url)
-                if emails:
-                    for email in emails:
-                        if uuid in self.results:
-                            self.results[uuid].add(email)
-                        else:
-                            self.results[uuid] = {email}
+        if homepage_url:
+            print(f"Processing {homepage_url}")
+            emails = self.extract_emails_from_url(homepage_url)
+            if emails:
+                for email in emails:
+                    if uuid in self.results:
+                        self.results[uuid].add(email)
+                    else:
+                        self.results[uuid] = {email}
+
+    def process_csv(self):
+        with ThreadPoolExecutor(max_workers=self.num_threads) as executor:
+            future_to_row = {executor.submit(self._process_row, row): row for row in self._data}
+
+            for future in as_completed(future_to_row):
+                row = future_to_row[future]
+                try:
+                    future.result()  # Retrieve any exceptions here
+                except Exception as e:
+                    print(f"Error processing row {row}: {e}")
 
     def get_result(self):
         return self.results
--- parser_helpers/chunkers/chunkers.py
+++ parser_helpers/chunkers/chunkers.py
@@ -12,8 +12,17 @@ class SimpleChunker(BaseChunker):
         self.num_processes = num_processes
 
     def chunk_data(self, data):
+        """Разбивает данные на примерно равные части для каждого процесса."""
         chunk_size = len(data) // self.num_processes
-        return [data[i : i + chunk_size] for i in range(0, len(data), chunk_size)]
+        chunks = [data[i : i + chunk_size] for i in range(0, len(data), chunk_size)]
+
+        # Если остались лишние элементы, распределяем их по чанкам
+        if len(chunks) > self.num_processes:
+            extra_data = chunks.pop()
+            for i, item in enumerate(extra_data):
+                chunks[i % len(chunks)].append(item)
+
+        return chunks
 
 
 class PercentageChunker(BaseChunker):
