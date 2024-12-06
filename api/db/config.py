9e67c6900bd3a504ee28b54bb234a8eba4e169d4
Illa Tsiluiko <helperfor@ukr.net>
2024-12-06 15:43:30 +0200

    add_env

--- api/db/config.py
+++ api/db/config.py
@@ -1,12 +1,17 @@
+import os
+
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-POSTGRES_DB = "citus"
-POSTGRES_USER = "citus"
-POSTGRES_PASSWORD = "z4peRnZYXSFwjkB"
-POSTGRES_HOST = "c-vc-board.sdp7gta73xhnts.postgres.cosmos.azure.com"
-POSTGRES_PORT = 5432
+load_dotenv()
+
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 
 DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
